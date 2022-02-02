16318441d24633d9926e84bc36814eb78c58993d
leesh125 <chelsea12345@naver.com>
2022-02-02 17:26:21 +0900

    Initial commit

---
+++ src/main/java/hello/springmvc_basic_function/SpringmvcBasicFunctionApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvc_basic_function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcBasicFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcBasicFunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springmvc_basic_function/SpringmvcBasicFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvc_basic_function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcBasicFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
